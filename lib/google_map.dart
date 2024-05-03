22a39845863437e0a4e85b9da34114de001ba91c
Mohammed Ezz <93770977+mohammedezz20@users.noreply.github.com>
2024-05-03 09:53:23 +0300

    google map camera ended

---
+++ lib/google_map.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CustomGoogleMap extends StatefulWidget {
+  const CustomGoogleMap({super.key});
+
+  @override
+  State<CustomGoogleMap> createState() => _CustomGoogleMapState();
+}
+
+class _CustomGoogleMapState extends State<CustomGoogleMap> {
+  //! only initial camera position
+  late CameraPosition initCameraPosition;
+  late GoogleMapController? mapController;
+
+  @override
+  void initState() {
+    initCameraPosition = const CameraPosition(
+      target: LatLng(30.356954, 30.530255),
+      zoom: 12,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    mapController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              onMapCreated: (controller) {
+                mapController = controller;
+              },
+              //! Set the initial camera position
+              initialCameraPosition: initCameraPosition,
+
+              //! Sets a frame for a specific area within the map
+              // cameraTargetBounds: CameraTargetBounds(
+              //   LatLngBounds(
+              //     southwest: const LatLng(30.325861, 30.524547),
+              //     northeast: const LatLng(30.500847, 30.536670),
+              //   ),
+              // ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          mapController!.animateCamera(
+                            CameraUpdate.newLatLng(
+                              const LatLng(30.046396, 31.224839),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: const Text('Cairo'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          mapController!.animateCamera(
+                            CameraUpdate.newLatLng(
+                              const LatLng(31.201009, 29.914291),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: const Text('Alexandria'),
+                  ),
+                ]),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps/google_map.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: CustomGoogleMap());
   }
 }
